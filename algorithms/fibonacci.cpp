ab6cde5b5242b50d2c17ec1674648a6d725c1c24
ryanty <42381062+ryanYtan@users.noreply.github.com>
2020-10-07 17:42:30 +0800

    Add files via upload

--- algorithms/fibonacci.cpp
+++ algorithms/fibonacci.cpp
@@ -1,46 +1,31 @@
 /**
- * Fibonacci Sequence follows:
- *    0,    1,    1,    2, 3, 5, 8, etc
- *    0th   1st   2nd   etc...
+ * Define F(0) = 1, F(1) = 1, F(n) = F(n - 1) + F(n - 2)
  */
 #include <iostream>
+#include <vector>
 
-using ull = unsigned long long;
-
-ull dp_fib(int n)
+int memo_fib(int n, std::vector<int>& memotable)
 {
-    if (n == 0)
-        return 0;
-    if (n == 1)
+    if (n == 0 || n == 1)
         return 1;
 
-    ull memo[n + 1];
-    memo[0] = 0;
-    memo[1] = 1;
-
-    for (int i = 2; i <= n; i++) {
-        memo[i] = memo[i - 1] + memo[i - 2];
-        if (memo[i] < memo[i - 1]) {
-            std::cerr << "Unsigned overflow occured. Exiting...\n";
-            exit(1);
-        }
+    if (memotable[n] == -1) {
+        memotable[n] = memo_fib(n - 1, memotable) + memo_fib(n - 2, memotable);
     }
-
-    return memo[n];
+    return memotable[n];
 }
 
-ull naive_fib(int n)
+int naive_fib(int n)
 {
-    if (n == 1)
-        return 1;
-    if (n == 2)
+    if (n == 0 || n == 1)
         return 1;
     return naive_fib(n - 1) + naive_fib(n - 2);
 }
 
-int main(void)
+int main()
 {
     int n;
     std::cin >> n;
-    std::cout << dp_fib(n) << "\n";
+    std::vector<int> memotable(n + 1, -1);
+    std::cout << memo_fib(n, memotable) << "\n";
 }
